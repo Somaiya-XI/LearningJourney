ecd402e587591cf3d8fa205146c437b877914f22
Somaiya <4111674@upm.edu.sa>
2025-10-19 14:43:58 +0300

    Initial Commit

---
+++ LearningJourney/LearningJourneyApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningJourneyApp.swift
+//  LearningJourney
+//
+//  Created by Somaiya on 27/04/1447 AH.
+//
+
+import SwiftUI
+
+@main
+struct LearningJourneyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
